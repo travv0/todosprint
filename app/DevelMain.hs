91e0544ff0833da216737a360b98c75d983d0078
Travis <tssund93@gmail.com>
2018-10-15 12:21:48 -0500

    not sure what to try at this point

--- app/DevelMain.hs
+++ app/DevelMain.hs
@@ -1,105 +1,104 @@
 -- | Running your app inside GHCi.
 --
--- This option provides significantly faster code reload compared to
--- @yesod devel@. However, you do not get automatic code reload
--- (which may be a benefit, depending on your perspective). To use this:
+-- To start up GHCi for usage with Yesod, first make sure you are in dev mode:
 --
--- 1. Start up GHCi
+-- > cabal configure -fdev
 --
--- $ stack ghci scheduler:lib --no-load --work-dir .stack-work-devel
+-- Note that @yesod devel@ automatically sets the dev flag.
+-- Now launch the repl:
 --
--- 2. Load this module
+-- > cabal repl --ghc-options="-O0 -fobject-code"
 --
--- > :l app/DevelMain.hs
+-- To start your app, run:
 --
--- 3. Run @update@
---
--- > DevelMain.update
---
--- 4. Your app should now be running, you can connect at http://localhost:3000
---
--- 5. Make changes to your code
---
--- 6. After saving your changes, reload by running:
---
--- > :r
+-- > :l DevelMain
 -- > DevelMain.update
 --
 -- You can also call @DevelMain.shutdown@ to stop the app
 --
+-- You will need to add the foreign-store package to your .cabal file.
+-- It is very light-weight.
+--
+-- If you don't use cabal repl, you will need
+-- to run the following in GHCi or to add it to
+-- your .ghci file.
+--
+-- :set -DDEVELOPMENT
+--
 -- There is more information about this approach,
 -- on the wiki: https://github.com/yesodweb/yesod/wiki/ghci
---
--- WARNING: GHCi does not notice changes made to your template files.
--- If you change a template, you'll need to either exit GHCi and reload,
--- or manually @touch@ another Haskell module.
 
 module DevelMain where
 
-import Prelude
-import Application (getApplicationRepl, shutdownApp)
+import           Prelude
+import           Application                    ( getApplicationRepl
+                                                , shutdownApp
+                                                )
 
-import Control.Monad ((>=>))
-import Control.Concurrent
-import Data.IORef
-import Foreign.Store
-import Network.Wai.Handler.Warp
-import GHC.Word
+import           Control.Exception              ( finally )
+import           Control.Monad                  ( (>=>) )
+import           Control.Concurrent
+import           Data.IORef
+import           Foreign.Store
+import           Network.Wai.Handler.Warp
+import           GHC.Word
 
 -- | Start or restart the server.
 -- newStore is from foreign-store.
 -- A Store holds onto some data across ghci reloads
 update :: IO ()
 update = do
-    mtidStore <- lookupStore tidStoreNum
-    case mtidStore of
-      -- no server running
-      Nothing -> do
-          done <- storeAction doneStore newEmptyMVar
-          tid <- start done
-          _ <- storeAction (Store tidStoreNum) (newIORef tid)
-          return ()
-      -- server is already running
-      Just tidStore -> restartAppInNewThread tidStore
-  where
-    doneStore :: Store (MVar ())
-    doneStore = Store 0
+  mtidStore <- lookupStore tidStoreNum
+  case mtidStore of
+    -- no server running
+    Nothing -> do
+      done <- storeAction doneStore newEmptyMVar
+      tid  <- start done
+      _    <- storeAction (Store tidStoreNum) (newIORef tid)
+      return ()
+    -- server is already running
+    Just tidStore -> restartAppInNewThread tidStore
+ where
+  doneStore :: Store (MVar ())
+  doneStore = Store 0
 
-    -- shut the server down with killThread and wait for the done signal
-    restartAppInNewThread :: Store (IORef ThreadId) -> IO ()
-    restartAppInNewThread tidStore = modifyStoredIORef tidStore $ \tid -> do
-        killThread tid
-        withStore doneStore takeMVar
-        readStore doneStore >>= start
+  -- shut the server down with killThread and wait for the done signal
+  restartAppInNewThread :: Store (IORef ThreadId) -> IO ()
+  restartAppInNewThread tidStore = modifyStoredIORef tidStore $ \tid -> do
+    killThread tid
+    withStore doneStore takeMVar
+    readStore doneStore >>= start
 
 
-    -- | Start the server in a separate thread.
-    start :: MVar () -- ^ Written to when the thread is killed.
-          -> IO ThreadId
-    start done = do
-        (port, site, app) <- getApplicationRepl
-        forkFinally
-            (runSettings (setPort port defaultSettings) app)
-            -- Note that this implies concurrency
-            -- between shutdownApp and the next app that is starting.
-            -- Normally this should be fine
-            (\_ -> putMVar done () >> shutdownApp site)
+  -- | Start the server in a separate thread.
+  start
+    :: MVar () -- ^ Written to when the thread is killed.
+    -> IO ThreadId
+  start done = do
+    (port, site, app) <- getApplicationRepl
+    forkIO
+      (finally (runSettings (setPort port defaultSettings) app)
+                    -- Note that this implies concurrency
+                    -- between shutdownApp and the next app that is starting.
+                    -- Normally this should be fine
+               (putMVar done () >> shutdownApp site)
+      )
 
 -- | kill the server
 shutdown :: IO ()
 shutdown = do
-    mtidStore <- lookupStore tidStoreNum
-    case mtidStore of
-      -- no server running
-      Nothing -> putStrLn "no Yesod app running"
-      Just tidStore -> do
-          withStore tidStore $ readIORef >=> killThread
-          putStrLn "Yesod app is shutdown"
+  mtidStore <- lookupStore tidStoreNum
+  case mtidStore of
+    -- no server running
+    Nothing       -> putStrLn "no Yesod app running"
+    Just tidStore -> do
+      withStore tidStore $ readIORef >=> killThread
+      putStrLn "Yesod app is shutdown"
 
 tidStoreNum :: Word32
 tidStoreNum = 1
 
 modifyStoredIORef :: Store (IORef a) -> (a -> IO a) -> IO ()
 modifyStoredIORef store f = withStore store $ \ref -> do
-    v <- readIORef ref
-    f v >>= writeIORef ref
+  v <- readIORef ref
+  f v >>= writeIORef ref
