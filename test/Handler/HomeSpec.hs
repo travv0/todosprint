a949e96dacbad86051b439f5f78ff6a108b0599e
Travis Sunderland <travis@sunderland.io>
2018-10-24 16:41:21 -0500

    adding tests

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -1,24 +1,56 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Handler.HomeSpec
-    ( spec
-    )
+  ( spec
+  )
 where
 
 import           TestImport
+import           Priority
+import           Data.Time
 
 spec :: Spec
 spec = withApp $ do
 
-    describe "Manage page" $ do
-        it "asserts redirect to login page from manage page for anonymous users"
-            $ do
-                  get HomeR
-                  statusIs 303
+  describe "Manage page" $ do
+    it "asserts redirect to login page from manage page for anonymous users"
+      $ do
+          get HomeR
+          statusIs 303
 
-        it "asserts access to manage page for authenticated users" $ do
-            userEntity <- createUser "foo@gmail.com"
-            authenticateAs userEntity
+    it "asserts access to manage page for authenticated users" $ do
+      userEntity <- createUser "foo@gmail.com"
+      authenticateAs userEntity
 
-            get HomeR
-            statusIs 200
+      get HomeR
+      statusIs 200
+
+    it "has detailed task list" $ do
+      currTime   <- liftIO getCurrentTime
+
+      userEntity <- createUser "foo@gmail.com"
+      runDB $ update (entityKey userEntity)
+                     [UserDueTime =. Just (addUTCTime 90 currTime)]
+
+      authenticateAs userEntity
+
+      testTask <- runDB $ insert $ Task "Test"
+                                        1
+                                        None
+                                        Nothing
+                                        Nothing
+                                        False
+                                        (entityKey userEntity)
+                                        Nothing
+                                        Nothing
+                                        currTime
+                                        Nothing
+                                        False
+                                        Nothing
+
+      get HomeR
+      statusIs 200
+
+      htmlCount ".task" 1
+      htmlAllContain ".taskDate"     "Priority"
+      htmlAllContain ".taskDuration" "minutes"
--- test/Handler/TodaySpec.hs
+++ test/Handler/TodaySpec.hs
@@ -348,3 +348,348 @@ spec = withApp $ do
         , "nonePriority"
         , "nonePriorityNoDueDate"
         ]
+
+    it "weighs tasks correctly relative to each other" $ do
+      currTime      <- liftIO getCurrentTime
+      currTimeLater <- liftIO getCurrentTime
+      let today = utctDay currTime
+
+      userEntity <- createUser "foo@gmail.com"
+      runDB $ update (entityKey userEntity)
+                     [UserDueTime =. Just (addUTCTime 90 currTime)]
+
+      authenticateAs userEntity
+
+      highPriority <- runDB $ insertEntity $ Task "highPriority"
+                                                  30
+                                                  High
+                                                  (Just today)
+                                                  Nothing
+                                                  False
+                                                  (entityKey userEntity)
+                                                  Nothing
+                                                  Nothing
+                                                  currTime
+                                                  Nothing
+                                                  False
+                                                  Nothing
+      highPriorityOverdue <- runDB $ insertEntity $ Task
+        "highPriorityOverdue"
+        30
+        High
+        (Just $ addDays (-1) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      highPriorityWayOverdue <- runDB $ insertEntity $ Task
+        "highPriorityWayOverdue"
+        30
+        High
+        (Just $ addDays (-9) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      highPriorityNoDueDate <- runDB $ insertEntity $ Task
+        "highPriorityNoDueDate"
+        30
+        High
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      mediumPriority <- runDB $ insertEntity $ Task "mediumPriority"
+                                                    30
+                                                    Medium
+                                                    (Just today)
+                                                    Nothing
+                                                    False
+                                                    (entityKey userEntity)
+                                                    Nothing
+                                                    Nothing
+                                                    currTime
+                                                    Nothing
+                                                    False
+                                                    Nothing
+      mediumPriorityOverdue <- runDB $ insertEntity $ Task
+        "mediumPriorityOverdue"
+        30
+        Medium
+        (Just $ addDays (-1) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      mediumPriorityWayOverdue <- runDB $ insertEntity $ Task
+        "mediumPriorityWayOverdue"
+        30
+        Medium
+        (Just $ addDays (-9) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      mediumPriorityNoDueDate <- runDB $ insertEntity $ Task
+        "mediumPriorityNoDueDate"
+        30
+        Medium
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      mediumPriorityNoDueDateShort <- runDB $ insertEntity $ Task
+        "mediumPriorityNoDueDateShort"
+        5
+        Medium
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      mediumPriorityNoDueDateShortAddedLater <- runDB $ insertEntity $ Task
+        "mediumPriorityNoDueDateShortAddedLater"
+        5
+        Medium
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTimeLater
+        Nothing
+        False
+        Nothing
+      lowPriority <- runDB $ insertEntity $ Task "lowPriority"
+                                                 30
+                                                 Low
+                                                 (Just today)
+                                                 Nothing
+                                                 False
+                                                 (entityKey userEntity)
+                                                 Nothing
+                                                 Nothing
+                                                 currTime
+                                                 Nothing
+                                                 False
+                                                 Nothing
+      lowPriorityOverdue <- runDB $ insertEntity $ Task
+        "lowPriorityOverdue"
+        30
+        Low
+        (Just $ addDays (-1) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      lowPriorityWayOverdue <- runDB $ insertEntity $ Task
+        "lowPriorityWayOverdue"
+        30
+        Low
+        (Just $ addDays (-9) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      lowPriorityNoDueDate <- runDB $ insertEntity $ Task
+        "lowPriorityNoDueDate"
+        30
+        Low
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      nonePriority <- runDB $ insertEntity $ Task "nonePriority"
+                                                  30
+                                                  None
+                                                  (Just today)
+                                                  Nothing
+                                                  False
+                                                  (entityKey userEntity)
+                                                  Nothing
+                                                  Nothing
+                                                  currTime
+                                                  Nothing
+                                                  False
+                                                  Nothing
+      nonePriorityOverdue <- runDB $ insertEntity $ Task
+        "nonePriorityOverdue"
+        30
+        None
+        (Just $ addDays (-1) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      nonePriorityWayOverdue <- runDB $ insertEntity $ Task
+        "nonePriorityWayOverdue"
+        30
+        None
+        (Just $ addDays (-900) today)
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+      nonePriorityNoDueDate <- runDB $ insertEntity $ Task
+        "nonePriorityNoDueDate"
+        30
+        None
+        Nothing
+        Nothing
+        False
+        (entityKey userEntity)
+        Nothing
+        Nothing
+        currTime
+        Nothing
+        False
+        Nothing
+
+      assertEq
+        "High priority should have higher weight than medium"
+        ( weight today (entityVal highPriority)
+        > weight today (entityVal mediumPriority)
+        )
+        True
+      assertEq
+        "Medium priority should have higher weight than low"
+        ( weight today (entityVal mediumPriority)
+        > weight today (entityVal lowPriority)
+        )
+        True
+      assertEq
+        "Low priority should have higher weight than none"
+        ( weight today (entityVal lowPriority)
+        > weight today (entityVal nonePriority)
+        )
+        True
+      assertEq
+        "Low priority overdue by longer than a week should have higher weight than medium"
+        ( weight today (entityVal lowPriorityWayOverdue)
+        > weight today (entityVal mediumPriority)
+        )
+        True
+      assertEq
+        "Medium priority overdue by longer than a week should not have higher weight than high"
+        ( weight today (entityVal mediumPriorityWayOverdue)
+        > weight today (entityVal highPriority)
+        )
+        False
+      assertEq
+        "None priority should never have higher weight than anything else"
+        ( weight today (entityVal nonePriorityWayOverdue)
+        > weight today (entityVal lowPriority)
+        )
+        False
+      assertEq
+        "High priority should be weighted by how overdue"
+        ( weight today (entityVal highPriorityWayOverdue)
+        > weight today (entityVal highPriorityOverdue)
+        )
+        True
+      assertEq
+        "Tasks with no due date should be weighted lower than those with due dates, unless none priority"
+        (  ( weight today (entityVal highPriority)
+           > weight today (entityVal highPriorityNoDueDate)
+           )
+        && ( weight today (entityVal mediumPriority)
+           > weight today (entityVal mediumPriorityNoDueDate)
+           )
+        && ( weight today (entityVal lowPriority)
+           > weight today (entityVal lowPriorityNoDueDate)
+           )
+        && (  weight today (entityVal nonePriority)
+           == weight today (entityVal nonePriorityNoDueDate)
+           )
+        )
+        True
+      assertEq
+        "None priority should always be the same weight as each other"
+        (all
+          (== 0)
+          (map
+            (weight today . entityVal)
+            [ nonePriority
+            , nonePriorityOverdue
+            , nonePriorityWayOverdue
+            , nonePriorityNoDueDate
+            ]
+          )
+        )
+        True
+      assertEq
+        "Task create date shouldn't affect weight"
+        (  weight today (entityVal mediumPriorityNoDueDateShort)
+        == weight today (entityVal mediumPriorityNoDueDateShortAddedLater)
+        )
+        True
+      assertEq
+        "Task duration shouldn't affect weight"
+        (  weight today (entityVal mediumPriorityNoDueDateShort)
+        == weight today (entityVal mediumPriorityNoDueDate)
+        )
+        True
