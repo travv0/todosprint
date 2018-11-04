bc2c3480009d5291c1c70cf9f15c04eea00d97ce
Travis <tssund93@gmail.com>
2018-11-03 23:13:58 -0500

    fixed and refactored add dependencies/dependents pages (#24)

--- src/Handler/AddDeps.hs
+++ src/Handler/AddDeps.hs
@@ -25,19 +25,37 @@ depsForm tasks label taskDeps =
              (Just taskDeps)
     <*  bootstrapSubmit ("Submit" :: BootstrapSubmit Text)
 
-getAddDepsR :: TaskId -> Handler Html
-getAddDepsR taskId = do
-  let optionify (Entity _taskId task) = (taskName task, taskId)
-  let getTaskDepId (Entity _tdid td) = taskDependencyDependsOnTaskId td
+data Dep = Dependency | Dependent
+  deriving (Eq, Show)
+
+addDeps :: Dep -> TaskId -> Handler Html
+addDeps dep taskId = do
+  let depField = case dep of
+        Dependency -> TaskDependencyTaskId
+        Dependent  -> TaskDependencyDependsOnTaskId
+  let optionify (Entity tid task) = (taskName task, tid)
+  let getTaskDepId Dependency (Entity _tdid td) =
+        taskDependencyDependsOnTaskId td
+      getTaskDepId Dependent (Entity _tdid td) = taskDependencyTaskId td
   userId                   <- requireAuthId
 
   tasks                    <- runDB $ getTasks userId taskId [Asc TaskName]
-  taskDeps <- runDB $ selectList [TaskDependencyTaskId ==. taskId] []
+  taskDeps                 <- runDB $ selectList [depField ==. taskId] []
 
   ((res, widget), enctype) <-
-    runFormPost $ depsForm (map optionify tasks) "Dependencies" $ Just $ map
-      getTaskDepId
-      taskDeps
+    runFormPost
+    $ depsForm (map optionify tasks)
+               (if dep == Dependency then "Dependencies" else "Dependents")
+    $ Just
+    $ map (getTaskDepId dep) taskDeps
+
+  let msg = case dep of
+        Dependency -> "Dependencies updated"
+        Dependent  -> "Dependents updated"
+
+  let depsR = case dep of
+        Dependency -> AddDepsR
+        Dependent  -> AddDependentsR
 
   mTask <- runDB $ Import.get taskId
   case mTask of
@@ -46,59 +64,34 @@ getAddDepsR taskId = do
         _ <- case deps ds of
           Just jds -> do
             runDB $ do
-              deleteWhere [TaskDependencyTaskId ==. taskId]
-              mapM (\d -> insert $ TaskDependency taskId d False) jds
+              deleteWhere [depField ==. taskId]
+              case dep of
+                Dependency ->
+                  mapM (\d -> insert $ TaskDependency taskId d False) jds
+                Dependent ->
+                  mapM (\d -> insert $ TaskDependency d taskId False) jds
           Nothing -> do
-            runDB $ deleteWhere [TaskDependencyTaskId ==. taskId]
-            setMessage "Dependencies updated"
+            runDB $ deleteWhere [depField ==. taskId]
+            setMessage msg
             redirect $ EditTaskR taskId
-        setMessage "Dependencies updated"
+        setMessage msg
         redirect $ EditTaskR taskId
       _ -> do
         defaultLayout $ do
-          setTitle $ toHtml $ "Add Dependencies for \"" ++ taskName task ++ "\""
-          [whamlet|<h3>Add Dependencies for "#{taskName task}"|]
+          setTitle $ toHtml $ "Add " ++ if dep == Dependency
+            then "Dependencies"
+            else "Dependents" ++ " for \"" ++ taskName task ++ "\""
           $(widgetFile "add-deps")
     Nothing -> redirectUltDest TodayR
+
+getAddDepsR :: TaskId -> Handler Html
+getAddDepsR = addDeps Dependency
 
 postAddDepsR :: TaskId -> Handler Html
 postAddDepsR = getAddDepsR
 
 getAddDependentsR :: TaskId -> Handler Html
-getAddDependentsR taskId = do
-  let optionify (Entity _taskId task) = (taskName task, taskId)
-  let getTaskDepId (Entity _tdid td) = taskDependencyTaskId td
-  userId                   <- requireAuthId
-
-  tasks                    <- runDB $ getTasks userId taskId [Asc TaskName]
-  taskDeps <- runDB $ selectList [TaskDependencyDependsOnTaskId ==. taskId] []
-
-  ((res, widget), enctype) <-
-    runFormPost $ depsForm (map optionify tasks) "Dependents" $ Just $ map
-      getTaskDepId
-      taskDeps
-
-  mTask <- runDB $ Import.get taskId
-  case mTask of
-    Just task -> case res of
-      FormSuccess ds -> do
-        _ <- case deps ds of
-          Just jds -> do
-            runDB $ do
-              deleteWhere [TaskDependencyDependsOnTaskId ==. taskId]
-              mapM (\d -> insert $ TaskDependency d taskId False) jds
-          Nothing -> do
-            runDB $ deleteWhere [TaskDependencyDependsOnTaskId ==. taskId]
-            setMessage "Dependents updated"
-            redirect $ EditTaskR taskId
-        setMessage "Dependents updated"
-        redirect $ EditTaskR taskId
-      _ -> do
-        defaultLayout $ do
-          setTitle $ toHtml $ "Add Dependents for \"" ++ taskName task ++ "\""
-          [whamlet|<h3>Add Dependents for "#{taskName task}"|]
-          $(widgetFile "add-dependents")
-    Nothing -> redirectUltDest TodayR
+getAddDependentsR = addDeps Dependent
 
 postAddDependentsR :: TaskId -> Handler Html
 postAddDependentsR = getAddDependentsR
