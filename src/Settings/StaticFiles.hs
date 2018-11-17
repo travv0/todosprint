d2e09f37fcf80b5568834245e8fdd558c1c588b1
Travis Sunderland <travis@sunderland.io>
2018-11-16 22:15:58 -0600

    remove warning

--- src/Settings/StaticFiles.hs
+++ src/Settings/StaticFiles.hs
@@ -6,9 +6,7 @@ module Settings.StaticFiles where
 import           Settings                       ( appStaticDir
                                                 , compileTimeAppSettings
                                                 )
-import           Yesod.Static                   ( staticFiles
-                                                , staticFilesList
-                                                )
+import           Yesod.Static                   ( staticFiles )
 
 -- This generates easy references to files in the static directory at compile time,
 -- giving you compile-time verification that referenced files exist.
