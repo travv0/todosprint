3395682af38450fafdf76c79f1200a2e0f3ee832
Travis <tssund93@gmail.com>
2019-01-25 23:10:07 -0600

    fix tests and remove no longer relevant ones

--- test/Handler/TaskSpec.hs
+++ test/Handler/TaskSpec.hs
@@ -10,234 +10,9 @@ import           Priority
 import           Data.Time
 import           Common
 import           Data.Maybe                    as M
-import qualified Data.List                     as L
 
 spec :: Spec
 spec = withApp $ do
-  describe "Add dependencies page" $ do
-    it "doesn't say dependent anywhere" $ do
-      currTime   <- liftIO getCurrentTime
-
-      userEntity <- createUser "foo@gmail.com"
-
-      authenticateAs userEntity
-
-      testTask <- runDB $ insert $ Task "Test"
-                                        1
-                                        High
-                                        Nothing
-                                        Nothing
-                                        Nothing
-                                        False
-                                        (entityKey userEntity)
-                                        Nothing
-                                        currTime
-                                        Nothing
-                                        False
-                                        Nothing
-                                        False
-
-      get $ AddDepsR testTask
-      statusIs 200
-
-      bodyNotContains "dependent"
-      bodyNotContains "Dependent"
-      bodyContains "Dependenc"
-
-    it "adds dependencies correctly" $ do
-      currTime   <- liftIO getCurrentTime
-
-      userEntity <- createUser "foo@gmail.com"
-
-      authenticateAs userEntity
-
-      testTask <- runDB $ insert $ Task "Test"
-                                        1
-                                        High
-                                        Nothing
-                                        Nothing
-                                        Nothing
-                                        False
-                                        (entityKey userEntity)
-                                        Nothing
-                                        currTime
-                                        Nothing
-                                        False
-                                        Nothing
-                                        False
-      depTask <- runDB $ insert $ Task "dep"
-                                       1
-                                       High
-                                       Nothing
-                                       Nothing
-                                       Nothing
-                                       False
-                                       (entityKey userEntity)
-                                       Nothing
-                                       currTime
-                                       Nothing
-                                       False
-                                       Nothing
-                                       False
-      _ <- runDB $ insert $ Task "nothing"
-                                 1
-                                 High
-                                 Nothing
-                                 Nothing
-                                 Nothing
-                                 False
-                                 (entityKey userEntity)
-                                 Nothing
-                                 currTime
-                                 Nothing
-                                 False
-                                 Nothing
-                                 False
-      _ <- runDB $ insert $ Task "nothing2"
-                                 1
-                                 High
-                                 Nothing
-                                 Nothing
-                                 Nothing
-                                 False
-                                 (entityKey userEntity)
-                                 Nothing
-                                 currTime
-                                 Nothing
-                                 False
-                                 Nothing
-                                 False
-
-      get $ AddDepsR testTask
-      statusIs 200
-
-      printBody
-
-      request $ do
-        setMethod "POST"
-        setUrl $ AddDepsR testTask
-        addToken
-        byLabelExact "Dependencies" "1"
-
-      deps <- runDB $ selectList [TaskDependencyTaskId ==. testTask] []
-      assertEq "One dependency" (length deps) 1
-      assertEq ("Correct dependency for " ++ show testTask)
-               (taskDependencyDependsOnTaskId $ entityVal $ L.head deps)
-               depTask
-
-  describe "Add dependents page" $ do
-    it "doesn't say dependency/dependencies anywhere" $ do
-      currTime   <- liftIO getCurrentTime
-
-      userEntity <- createUser "foo@gmail.com"
-
-      authenticateAs userEntity
-
-      testTask <- runDB $ insert $ Task "Test"
-                                        1
-                                        High
-                                        Nothing
-                                        Nothing
-                                        Nothing
-                                        False
-                                        (entityKey userEntity)
-                                        Nothing
-                                        currTime
-                                        Nothing
-                                        False
-                                        Nothing
-                                        False
-
-      get $ AddDependentsR testTask
-      statusIs 200
-
-      printBody
-
-      bodyNotContains "dependenc"
-      bodyNotContains "Dependenc"
-      bodyContains "Dependent"
-
-    it "adds dependents correctly" $ do
-      currTime   <- liftIO getCurrentTime
-
-      userEntity <- createUser "foo@gmail.com"
-
-      authenticateAs userEntity
-
-      testTask <- runDB $ insert $ Task "Test"
-                                        1
-                                        High
-                                        Nothing
-                                        Nothing
-                                        Nothing
-                                        False
-                                        (entityKey userEntity)
-                                        Nothing
-                                        currTime
-                                        Nothing
-                                        False
-                                        Nothing
-                                        False
-      depTask <- runDB $ insert $ Task "dep"
-                                       1
-                                       High
-                                       Nothing
-                                       Nothing
-                                       Nothing
-                                       False
-                                       (entityKey userEntity)
-                                       Nothing
-                                       currTime
-                                       Nothing
-                                       False
-                                       Nothing
-                                       False
-      _ <- runDB $ insert $ Task "nothing"
-                                 1
-                                 High
-                                 Nothing
-                                 Nothing
-                                 Nothing
-                                 False
-                                 (entityKey userEntity)
-                                 Nothing
-                                 currTime
-                                 Nothing
-                                 False
-                                 Nothing
-                                 False
-      _ <- runDB $ insert $ Task "nothing2"
-                                 1
-                                 High
-                                 Nothing
-                                 Nothing
-                                 Nothing
-                                 False
-                                 (entityKey userEntity)
-                                 Nothing
-                                 currTime
-                                 Nothing
-                                 False
-                                 Nothing
-                                 False
-
-      get $ AddDependentsR testTask
-      statusIs 200
-
-      printBody
-
-      request $ do
-        setMethod "POST"
-        setUrl $ AddDependentsR testTask
-        addToken
-        byLabelExact "Dependents" "1"
-
-      deps <- runDB $ selectList [TaskDependencyDependsOnTaskId ==. testTask] []
-      assertEq "One dependency" (length deps) 1
-      assertEq ("Correct dependent for " ++ show testTask)
-               (taskDependencyTaskId $ entityVal $ L.head deps)
-               depTask
-
   describe "New task page" $ do
     it "successfully adds tasks" $ do
       userEntity' <- createUser "foo@gmail.com"
@@ -259,7 +34,6 @@ spec = withApp $ do
         byLabelExact "Duration in Minutes" "4"
         byLabelExact "Priority"            "4"
         byLabelExact "Due Date"            "2008-11-03"
-        byLabelExact "Start Time"          "23:00"
 
       statusIs 303
       r <- getResponse
@@ -276,12 +50,6 @@ spec = withApp $ do
             4
             High
             (fromGregorianValid 2008 11 3)
-            (   localTimeToUTC userTz
-            <$> (   LocalTime
-                <$> fromGregorianValid 2008 11 3
-                <*> makeTimeOfDayValid 23 0 0
-                )
-            )
             Nothing
             False
             (entityKey userEntity)
@@ -290,7 +58,6 @@ spec = withApp $ do
             Nothing
             False
             Nothing
-            True
 
   describe "Marking task done" $ do
     it "works for repeating task by days" $ do
@@ -305,7 +72,6 @@ spec = withApp $ do
         1
         High
         (Just today)
-        (Just (addUTCTime 59 currTime))
         Nothing
         False
         (entityKey userEntity)
@@ -314,7 +80,6 @@ spec = withApp $ do
         Nothing
         False
         Nothing
-        False
 
       request $ do
         setMethod "POST"
